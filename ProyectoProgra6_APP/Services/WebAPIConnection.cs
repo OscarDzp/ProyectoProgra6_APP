25fd9656b8dd2d98276440d9e876efd6db2f85d8
Oscar <ozumbado2000@gmail.com>
2024-03-23 11:15:39 -0600

    Semana 9 lista

---
+++ ProyectoProgra6_APP/Services/WebAPIConnection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoProgra6_APP.Services
+{
+    public static class WebAPIConnection
+    {
+        public static string ProductionURLPrefix = "http://192.168.100.2:45455/api/";
+
+        public static string TestingURLPrefix = "http://192.168.100.2:45455/api/";
+
+        public static string ApiKeyName = "P6ApiKey";
+
+        public static string ApiKeyValue = "autosmagnate";
+    }
+}
